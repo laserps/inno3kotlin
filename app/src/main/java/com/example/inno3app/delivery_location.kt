564e61ca12ed7b00d948a7f8802a72ca034ce8f8
haritleksopee <park.prim123@gmail.com>
2019-02-05 16:59:24 +0700

    update page content_confirm_order.xml page delivery_location.kt page history_order.kt

---
+++ app/src/main/java/com/example/inno3app/delivery_location.kt
@@ -0,0 +1,18 @@
+package com.example.inno3app
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity;
+
+import kotlinx.android.synthetic.main.activity_delivery_location.*
+
+class delivery_location : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_delivery_location)
+        setSupportActionBar(toolbar)
+
+    }
+
+}
