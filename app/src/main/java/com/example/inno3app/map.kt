e1cb7fa9158f0cf8b99ac672ccae5f87d2933447
haritleksopee <park.prim123@gmail.com>
2019-02-11 13:45:13 +0700

    add page google map on app

--- app/src/main/java/com/example/inno3app/map.kt
+++ app/src/main/java/com/example/inno3app/map.kt
@@ -10,13 +10,14 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
+
 class map : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_map)
+        setContentView(R.layout.activity_google)
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
