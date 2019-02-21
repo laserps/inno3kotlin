b49aa1262e6f607e68dab1e0f9b67d225831a452
haritleksopee <park.prim123@gmail.com>
2019-02-21 14:09:30 +0700

    add page payment

---
+++ app/src/main/java/com/example/inno3app/PaymentActivity.kt
@@ -0,0 +1,64 @@
+package com.example.inno3app
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity;
+import android.view.View
+import android.widget.RadioButton
+
+import kotlinx.android.synthetic.main.activity_payment.*
+import kotlinx.android.synthetic.main.content_payment.*
+
+class PaymentActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_payment)
+        setSupportActionBar(toolbar)
+        mybanking.visibility = View.GONE
+        mypaypal.visibility = View.GONE
+        radio_credit.setBackgroundResource(R.drawable.solid_checked)
+        radio_banking.setBackgroundResource(R.drawable.solid_notchecked)
+        radio_paypal.setBackgroundResource(R.drawable.solid_notchecked)
+    }
+    fun  onRadioButtonClicked(view: View) {
+        if (view is RadioButton) {
+            // Is the button now checked?
+            val checked = view.isChecked
+
+
+            // Check which radio button was clicked
+            when (view.getId()) {
+                R.id.radio_credit ->
+                    if (checked) {
+                        mycredit.visibility = View.VISIBLE
+                        mybanking.visibility = View.GONE
+                        mypaypal.visibility = View.GONE
+                        radio_credit.setBackgroundResource(R.drawable.solid_checked)
+                        radio_banking.setBackgroundResource(R.drawable.solid_notchecked)
+                        radio_paypal.setBackgroundResource(R.drawable.solid_notchecked)
+                    }
+                R.id.radio_banking ->
+                    if (checked) {
+                        mycredit.visibility = View.GONE
+                        mybanking.visibility = View.VISIBLE
+                        mypaypal.visibility = View.GONE
+                        radio_credit.setBackgroundResource(R.drawable.solid_notchecked)
+                        radio_banking.setBackgroundResource(R.drawable.solid_checked)
+                        radio_paypal.setBackgroundResource(R.drawable.solid_notchecked)
+                    }
+                R.id.radio_paypal ->
+                    if (checked) {
+                        mycredit.visibility = View.GONE
+                        mybanking.visibility = View.GONE
+                        mypaypal.visibility = View.VISIBLE
+                        radio_credit.setBackgroundResource(R.drawable.solid_notchecked)
+                        radio_banking.setBackgroundResource(R.drawable.solid_notchecked)
+                        radio_paypal.setBackgroundResource(R.drawable.solid_checked)
+                    }
+            }
+        }
+    }
+
+
+}
