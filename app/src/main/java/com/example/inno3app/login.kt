63289ebea1b525fcc99d24a003c478525c03cb8a
haritleksopee <park.prim123@gmail.com>
2019-02-22 09:50:19 +0700

    edit kotlin login.kt

--- app/src/main/java/com/example/inno3app/login.kt
+++ app/src/main/java/com/example/inno3app/login.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.content.Intent
+import android.view.Window
+import android.view.WindowManager
 import android.widget.Toast
 
 
@@ -13,7 +15,13 @@ class login : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        val actionBar = supportActionBar
+        actionBar!!.hide()
         setContentView(R.layout.activity_login)
+
 
         val btnOpenActivity : Button = findViewById(R.id.btn_link_username)
         btnOpenActivity.setOnClickListener{
@@ -35,14 +43,13 @@ class login : AppCompatActivity() {
 
 
     }
-
+//
     override fun onWindowFocusChanged(hasFocus: Boolean) {
         super.onWindowFocusChanged(hasFocus)
         if (hasFocus) hideSystemUI()
     }
     private fun hideSystemUI() {
         window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE
-                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                 or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                 or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                 or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
